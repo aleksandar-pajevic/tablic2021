3ff1e74025e6c6065a84aeb0e525409b56a92872
aleksandar-pajevic <aleksandar.pajevic@gmail.com>
2021-10-23 01:46:39 +0200

    game logic loop bug fixed

--- back/gameLogic.js
+++ back/gameLogic.js
@@ -50,28 +50,38 @@ function takeCards(cards, card) {
     }
   }
 
-  if (values[values.length - 1] > cardValue) {
+  if (values.length === 0) {
+    return false;
   }
-
+  console.log('values array:', valuesArr);
   for (niz of valuesArr) {
     let sum = 0;
     let addedNums = [];
     //first loop
-    for (let x = niz.length - 1; x >= 0; x--) {
+    for (let x = niz.length - 1; x > 0; x--) {
+      console.log('1st loop niz:', niz);
+      console.log('1st number:', niz[x]);
+      console.log('card value:', cardValue);
       addedNums.push(niz[x]);
       sum += niz[x];
-
+      console.log('1st loop sum:', sum);
       if (sum === cardValue) {
         niz = arrFilter(niz, addedNums);
         sum = 0;
+        console.log('removed numbers:', addedNums);
+        console.log('1nd loop niz reduced:', niz);
         addedNums = [];
+
         x = niz.length;
       } else if (sum > cardValue) {
       } else {
         //second loop
-        for (let y = 0; y < niz.length; y++) {
+        for (let y = 0; y < niz.length - 1; y++) {
+          console.log('2nd loop niz:', niz);
+          console.log('2nd loop number:', niz[y]);
           sum += niz[y];
           addedNums.push(niz[y]);
+          console.log('2nd loop sum:', sum);
 
           if (sum > cardValue) {
             sum = 0;
@@ -80,14 +90,18 @@ function takeCards(cards, card) {
           } else if (sum === cardValue) {
             niz = arrFilter(niz, addedNums);
             sum = 0;
+            console.log('removed numbers:', addedNums);
+            console.log('2nd loop niz reduced:', niz);
             addedNums = [];
             x = niz.length;
+
             break;
           }
         }
       }
     }
   }
+  console.log(valuesArr);
   let lengths = [];
   for (let m = 0; m < valuesArr.length; m++) {
     lengths.push(valuesArr[m].length);
@@ -99,3 +113,47 @@ function takeCards(cards, card) {
     return false;
   }
 }
+const filterTable = (table, selected) => {
+  return table.filter((item) => !selected.find((el) => item.code === el.code));
+};
+
+exports.takeCards = takeCards;
+exports.filterTable = filterTable;
+
+let test = [
+  {
+    value: '8',
+  },
+  {
+    value: '6',
+  },
+  {
+    value: '7',
+  },
+  {
+    value: '7',
+  },
+  {
+    value: '10',
+  },
+  {
+    value: '2',
+  },
+  {
+    value: '2',
+  },
+  {
+    value: '6',
+  },
+  {
+    value: '3',
+  },
+  {
+    value: '5',
+  },
+];
+
+let test2 = {
+  value: 'KING',
+};
+console.log(takeCards(test, test2));
