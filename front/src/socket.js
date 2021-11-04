2f4e6b9690cf308fe99f9fa518210a5c7fa5119e
aleksandar-pajevic <aleksandar.pajevic@gmail.com>
2021-11-04 19:11:47 +0100

    heroku endpoint

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-let PORT = 'http://127.0.0.1:5000';
+let PORT = 'https://tablic-back.herokuapp.com/';
 
 let socket = io(PORT, { transports: ['websocket', 'polling', 'flashsocket'] });
 
