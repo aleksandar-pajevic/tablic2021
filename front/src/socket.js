9c4c51ec99a852e19e3ac7d9c08cf7847686472e
aleksandar-pajevic <aleksandar.pajevic@gmail.com>
2021-11-06 14:15:55 +0100

    removing unused dependencies, localhost port for socket and ignore buind folder

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
-let PORT = 'https://tablic-back.herokuapp.com/';
+// let PORT = 'https://tablic-back.herokuapp.com/';
+let PORT = 'http://127.0.0.1:5000';
 
 let socket = io(PORT, { transports: ['websocket', 'polling', 'flashsocket'] });
 
